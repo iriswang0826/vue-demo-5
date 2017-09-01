ed950a47a7b434158b2b7aec76a97b2865c202d8
Iris Wang <iriswang1224@163.com>
2017-09-01 15:13:33 +0800

    过渡/动画

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <!-- 过渡/动画 -->
+    <button v-on:click="show = !show">
+      Toggle
+    </button>
+    <div class="ab">
+      <!-- <transition name="fade">
+        <p v-show="show">i am show</p>
+      </transition> -->
+      <transition name="my-trans">
+        <p v-show="show">i am show</p>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Vue from 'vue'
+import ComA from './components/A'
+export default {
+  components: {
+    ComA
+  },
+  data () {
+    return {
+      show: true
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+body {
+  display: flex;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: all .5s ease-out;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in below version 2.1.8 */ {
+  opacity: 0;
+}
+.my-trans-enter-active, .my-trans-leave-active {
+  transition: all .5s ease-out;
+}
+.my-trans-enter {
+  transform: translateY(-500px);
+  opacity: 0;
+}
+.my-trans-leave {
+  transform: translateY(500px);
+  opacity: 0;
+}
+</style>
---
+++ src/components/A.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{hello}}
+    {{myValue}}
+    <button @click="emitMyEvent">emit</button>
+    <!-- <slot>no slot</slot> -->
+    <slot name="header">no header</slot>
+    <p>乱七八糟</p>
+    <slot name="footer">no footer</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    'my-value': [Number, String, Object]
+  },
+  data () {
+    return {
+      hello: 'i am component a'
+    }
+  },
+  methods: {
+    emitMyEvent () {
+      this.$emit('my-event', this.hello)
+    }
+  }
+}
+</script>
